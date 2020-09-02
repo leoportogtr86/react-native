6a85c1791f34cda0f5b9c81f9789c24b65873755
leoporto <leonardoreisporto@gmail.com>
2020-09-02 06:51:00 -0300

    1

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './src/components/Button'
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+
+      <Text style = {styles.text}>{1 + 1}</Text>
+      <Button style = {styles.button} title = "Click 1"/>
+      <Button title = "Click 2"/>
+      <Button title = "Click 3"/>
+      <Button title = "Click 4"/>
+      <Button title = "Click 5"/>
+      <Button title = "Click 6"/>
+      <Button title = "Click 7"/>
+      
+      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +29,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+
+  text:{
+
+    fontSize: 200,
+    color: "yellow",
+    fontWeight: 'bold'
+    
+  },
+
+  button:{
+
+    backgroundColor: "red"
+  }
 });
